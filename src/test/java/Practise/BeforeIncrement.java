3e30f9a2e676c3c27a996be175e2e9f93c5b7e34
unknown <kranthidevavarapu@gmail.com>
2018-04-25 15:52:04 -0400

    increment

---
+++ src/test/java/Practise/BeforeIncrement.java
@@ -0,0 +1,15 @@
+package Practise;
+
+import org.junit.Test;
+
+public class BeforeIncrement {
+    @Test
+
+    public void BInc(){
+        int x;
+        x=12;
+        x+=1;
+        System.out.print("value of x is " +x);
+
+    }
+}
