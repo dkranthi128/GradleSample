f51c325dff29362bcfa0589c7a2eaea9f5f02b46
unknown <kranthidevavarapu@gmail.com>
2018-04-25 21:09:17 -0400

    files

---
+++ src/test/java/Practise/example.java
@@ -0,0 +1,15 @@
+package Practise;
+
+import org.junit.Test;
+
+public class example {
+    @Test
+    public void xxx(){
+
+        int x;
+
+        x=+1;
+        System.out.println("print the value " +x);
+
+        }
+}
